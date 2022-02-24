288cb4422d60ea66407850afcfd9bf49b9aa947e
a.lamour <dev.inf@hotmail.fr>
2022-02-24 12:04:24 +0100

    updates to deploy  to heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/poliscrypts-frontend/index.html'));
+app.use(express.static(__dirname + '/dist/poliscrypts-frontend'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/poliscrypts-frontend/index.html'));
+    res.sendFile(path.join(__dirname, 'poliscrypts-frontend', 'index.html'));
 })
 
-app.listen(process.env.port || 8080);
+app.listen(process.env.PORT || 5000);
